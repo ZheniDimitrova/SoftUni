84ebeb2c3f13dc6686485cdf46c4d985d68bdd08
ZheniDi <zheni.di.va@gmail.com>
2023-03-07 19:20:46 +0200

    EncapsulationExercise

---
+++ OOP/EncapsulationEx/ShoppingSpree_03/Main.java
@@ -0,0 +1,12 @@
+package EncapsulationEx.ShoppingSpree_03;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
