66271f8bfa9465d8b1543edca30ed0326f0cfd74
ZheniDi <zheni.di.va@gmail.com>
2023-08-25 10:32:08 +0300

    ex

---
+++ Data_Structures_Java/ds_fundamentals/linear-Data-Structures-Exercises/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import implementations.ReversedList;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
---
+++ Data_Structures_Java/ds_fundamentals/linear-Data-Structures-Exercises/src/main/java/implementations/BalancedParentheses.java
@@ -0,0 +1,29 @@
+package implementations;
+
+import interfaces.Solvable;
+
+public class BalancedParentheses implements Solvable {
+    private String parentheses;
+
+    public BalancedParentheses(String parentheses) {
+        this.parentheses = parentheses;
+    }
+
+    @Override
+    public Boolean solve() {
+        char[] input = parentheses.toCharArray();
+
+        for(int i = 0, j = input.length-1; i < j; i++, j--) {
+            char first = input[i];
+            char last = input[j];
+
+            if ((first == '(' && last != ')') || (first == '{' && last != '}') || (first != '[' && last == ']')) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+// Judge - 75p.
